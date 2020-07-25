c3ee9a702ee64f7c9393e9a9a7a2eec4e11d6d36
Anthony Cobb <anthonydcobb@gmail.com>
2020-07-25 16:38:19 +0000

    add insert method

--- index.js
+++ index.js
@@ -10,5 +10,37 @@ function HashNode(key, value, next){
   this.next = next || null;
 }
 
+HashTable.prototype.hash = function(key) {
+  let result = 0
+  for (letter in key) {
+    result += key.charCodeAt(letter)
+  }
+  //this makes the hash a number that fits the array size
+   return result % this.numBuckets;
+  
+}
+
+HashTable.prototype.insert = function(key, value) {
+  let index = this.hash(key);
+  if (!this.buckets[index]) {
+    this.buckets[index] = new HashNode(key, value)
+  } else {
+    let currentNode = this.buckets[index];
+    while (currentNode.next) {
+      currentNode = currentNode.next
+    }
+    currentNode.next = new HashNode(key, value)
+    
+  }
+  
+}
+
+
 let myHT = new HashTable(30);
+
+
+myHT.insert('anthony', 'ant@gmail.com')
+myHT.insert('nikki', 'nikki@gmail.com')
+myHT.insert('nikik', 'nikki@gmail.com')
+
 console.log(myHT)
