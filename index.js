79a324a5973046565ad6490913d71dfe524b975b
Anthony Cobb <anthonydcobb@gmail.com>
2020-07-25 02:03:56 +0000

    add HashNode function

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ function HashTable(size) {
   this.numBuckets = this.buckets.length;
 }
 
+function HashNode(key, value, next){
+  this.key = key;
+  this.value = value;
+  //this is incase of collisons
+  this.next = next || null;
+}
+
 let myHT = new HashTable(30);
 console.log(myHT)
