73e3d29eccbdce1715e59f48e8faaae928e64b49
Anthony Cobb <anthonydcobb@gmail.com>
2020-07-26 15:19:06 +0000

    refactor insert function

--- index.js
+++ index.js
@@ -24,9 +24,19 @@ HashTable.prototype.insert = function(key, value) {
   let index = this.hash(key);
   if (!this.buckets[index]) {
     this.buckets[index] = new HashNode(key, value)
+  } else if (this.buckets[index].key === key){  
+          this.buckets[index].value = value;
+        //you need the return statement to stop the function from continuing
+        return
   } else {
     let currentNode = this.buckets[index];
     while (currentNode.next) {
+      // this is to update a value if a key inserted is the same
+      if (currentNode.next.key === key) {
+        currentNode.next.value = value;
+        //you need the return statement to stop the function from continuing
+        return
+      }
       currentNode = currentNode.next
     }
     currentNode.next = new HashNode(key, value)
@@ -41,6 +51,16 @@ let myHT = new HashTable(30);
 
 myHT.insert('anthony', 'ant@gmail.com')
 myHT.insert('nikki', 'nikki@gmail.com')
-myHT.insert('nikik', 'nikki@gmail.com')
+myHT.insert('nikik', 'nikik@gmail.com')
+myHT.insert('anthony', 'ant2222@gmail.com')
+myHT.insert('hank', 'hank@gmail.com')
+myHT.insert('jo', 'jo@gmail.com')
+myHT.insert('boop', 'boop@gmail.com')
+myHT.insert('snoopy', 'snoop@gmail.com')
+myHT.insert('sonopy', 'snopo@gmail.com')
+myHT.insert('arrggg', 'a@gmail.com')
+myHT.insert('jim', 'b@gmail.com')
+myHT.insert('slim', 'bigguy@gmail.com')
+
 
 console.log(myHT)
